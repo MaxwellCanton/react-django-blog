7d088e0d86c7605a392c10b6a47d96bfdcf4cba7
Maxwell Canton <devmaxwellcanton@gmail.com>
2023-05-25 15:54:10 -0600

    added main frontend - header, body and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export function BackgroundPanel({}){
+    return (
+        <div style={{boxShadow : "0 1px 5px rgba(85, 85, 85, 0.15)", padding:"5%", marginBottom:"10%"}} >
+            
+        </div>
+    );
+};
