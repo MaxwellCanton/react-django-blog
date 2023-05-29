82172d01aea9070062856e4eedd74d328bb6303a
Maxwell Canton <devmaxwellcanton@gmail.com>
2023-05-28 21:43:57 -0600

    added footer component

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export function FooterComponent({}){
+    return (
+        <div className="footer"><a>Maxwell Hernandez - devmaxwellcanton@gmail.com</a></div>
+    );
+};
