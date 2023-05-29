17ab694f1a3348cded9ab369aace66c6728d9685
Maxwell Canton <devmaxwellcanton@gmail.com>
2023-05-28 21:54:50 -0600

    added component to handle 404 page not found

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,15 @@
+
+export function PageNotFound({}){
+
+
+    return (
+        <div className='container'>
+            <div className='main-panel'>
+                ups! this page does not exist !
+            </div>
+        </div>
+
+    );
+
+};
+
